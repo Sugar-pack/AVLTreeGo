84c5c674e7df581e61ab4defe78e31b4ccc52d9e
timur_akhmadiev <Timur_Akhmadiev@epam.com>
2022-04-04 11:57:54 +0300

    Initial commit

--- avltree_test.go
+++ avltree_test.go
@@ -12,5 +12,11 @@ func TestAVLTree_Append(t *testing.T) {
 	assert.Equal(t, 1, avlTree.Height)
 
 	avlTree.Append(2)
+	assert.Equal(t, V(1), avlTree.root.value)
+	assert.Equal(t, V(2), avlTree.root.right.root.value)
 
+	avlTree.Append(3)
+	assert.Equal(t, V(2), avlTree.root.value)
+	assert.Equal(t, V(1), avlTree.root.left.root.value)
+	assert.Equal(t, V(3), avlTree.root.right.root.value)
 }
